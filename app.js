315dc4786fd3149b04a48d5704ebc3d85c3fa967
starxmaker <beneath.black.skies@gmail.com>
2020-10-25 01:42:47 -0300

    se me olvidó agregar los nuevos headers y métodos permitidos

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(cors({
 
  app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, access-token, Content-Length,  Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE, PATCH');
     res.header('Access-Control-Allow-Credentials', true);
     next();
 })
