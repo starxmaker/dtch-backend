6806ec13995bb4201926f467daf94b9ff56c0c9d
starxmaker <beneath.black.skies@gmail.com>
2020-10-26 15:28:02 -0300

    oops

--- middlewares/jwt_auth.js
+++ middlewares/jwt_auth.js
@@ -5,7 +5,7 @@ const jwt_auth = async (req, res, next) => {
   //const token = req.cookies['token'] || '';
   const token2= req.headers['access-token'] || ''
   try {
-    if (!token) {
+    if (!token2) {
       return res.status(401).json('Por favor inicie sesión')
     }
     //const decrypt = await jwt.verify(token, process.env.JWT_KEY);
