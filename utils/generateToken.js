8ad2f9e4c6a350459d54564f71f8f26c301e9af1
starxmaker <beneath.black.skies@gmail.com>
2020-10-26 15:51:02 -0300

    reducción de tiempo de credenciales

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');
 require("dotenv/config")
 
 const generateToken = (res, id, username) => {
-  const expiration = 604800000;
+  const expiration = 10800;
   const token = jwt.sign({ id}, process.env.JWT_KEY, {
-    expiresIn: '7d',
+    expiresIn: '3h',
   });
   const token_header = jwt.sign({username }, process.env.JWT_KEY_HEADER, {
-    expiresIn: '7d',
+    expiresIn: '3h',
   });
   res.cookie('token', token, {
     expires: new Date(Date.now() + expiration),
