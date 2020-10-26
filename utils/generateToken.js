2db7c98c545180d3f2be28da37dcd76ebe7148ce
starxmaker <beneath.black.skies@gmail.com>
2020-10-26 16:06:35 -0300

    un poco más

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');
 require("dotenv/config")
 
 const generateToken = (res, id, username) => {
-  const expiration = 10800;
+  const expiration = 21600;
   const token = jwt.sign({ id}, process.env.JWT_KEY, {
-    expiresIn: '3h',
+    expiresIn: '6h',
   });
   const token_header = jwt.sign({username }, process.env.JWT_KEY_HEADER, {
-    expiresIn: '3h',
+    expiresIn: '6h',
   });
   res.cookie('token', token, {
     expires: new Date(Date.now() + expiration),
